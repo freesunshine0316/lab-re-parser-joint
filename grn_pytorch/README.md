65d23cef80531c0bbb7dd4f34da8fde914250d5e
/bin/eash <freesunshine0316@gmail.com>
2019-12-17 16:22:20 +0800

    remove unused code

--- grn_pytorch/README.md
+++
@@ -1,1 +0,0 @@
-# Source codes of GRN-based relation extraction system
--- grn_pytorch/data/5_concat_data.py
+++
@@ -1,8 +0,0 @@
-
-import os, sys, codecs, json
-
-data = []
-data += json.load(codecs.open('dev.json', 'rU', 'utf-8'))
-data += json.load(codecs.open('train.json', 'rU', 'utf-8'))
-json.dump(data, codecs.open('dev_train.json', 'w', 'utf-8'))
-
